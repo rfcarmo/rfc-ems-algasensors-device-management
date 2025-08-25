86eaa3df509d7f6ea9a63c49bc33dc44f181176d
rfcarmo <rfortunatoc@gmail.com>
2025-08-25 15:48:18 +0100

    10.9. Desafio: atualização e remoção de Sensor | 10.10. Desafio: inativação dos sensores

--- src/main/java/com/algaworks/algasensors/devicemanagement/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/devicemanagement/api/controller/SensorController.java
@@ -54,6 +54,51 @@ public class SensorController {
         return convertToModel(sensor);
     }
 
+    @PutMapping("/{sensorId}")
+    public SensorOutput update(@PathVariable TSID sensorId, @RequestBody SensorInput input) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+
+        sensor.setName(input.getName());
+        sensor.setIp(input.getIp());
+        sensor.setLocation(input.getLocation());
+        sensor.setProtocol(input.getProtocol());
+        sensor.setModel(input.getModel());
+
+        sensor = sensorRepository.saveAndFlush(sensor);
+
+        return convertToModel(sensor);
+    }
+
+    @DeleteMapping("/{sensorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+
+        sensorRepository.delete(sensor);
+    }
+
+    @PutMapping("/{sensorId}/enable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+
+        sensor.setEnabled(true);
+        sensorRepository.saveAndFlush(sensor);
+    }
+
+    @DeleteMapping("/{sensorId}/disable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disable(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+
+        sensor.setEnabled(false);
+        sensorRepository.saveAndFlush(sensor);
+    }
+
     private SensorOutput convertToModel(Sensor sensor) {
         return SensorOutput.builder()
                 .id(sensor.getId().getValue())
--- src/main/java/com/algaworks/algasensors/devicemanagement/api/model/SensorInput.java
+++ src/main/java/com/algaworks/algasensors/devicemanagement/api/model/SensorInput.java
@@ -1,14 +1,24 @@
 package com.algaworks.algasensors.devicemanagement.api.model;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class SensorInput {
 
+    @NonNull
     private String name;
+
+    @NonNull
     private String ip;
+
+    @NonNull
     private String location;
+
+    @NonNull
     private String protocol;
+
+    @NonNull
     private String model;
 
 }
