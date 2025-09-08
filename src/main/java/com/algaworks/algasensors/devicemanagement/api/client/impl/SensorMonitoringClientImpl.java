f34fd7f28d82581af4335e2fb151f11d52c2d267
rfcarmo <rfortunatoc@gmail.com>
2025-09-08 12:28:24 +0100

    11.3. HTTP Status Codes para erros na integração

--- src/main/java/com/algaworks/algasensors/devicemanagement/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/devicemanagement/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,7 +1,9 @@
 package com.algaworks.algasensors.devicemanagement.api.client.impl;
 
 import com.algaworks.algasensors.devicemanagement.api.client.SensorMonitoringClient;
+import com.algaworks.algasensors.devicemanagement.api.client.SensorMonitoringClientBadGatwayException;
 import io.hypersistence.tsid.TSID;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -11,7 +13,11 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
     private final RestClient restClient;
 
     public SensorMonitoringClientImpl(RestClient.Builder restClientBuilder) {
-        this.restClient = restClientBuilder.baseUrl("http://localhost:8082").build();
+        this.restClient = restClientBuilder.baseUrl("http://localhost:8082")
+                .defaultStatusHandler(HttpStatusCode::isError, ((request, response) -> {
+                    throw new SensorMonitoringClientBadGatwayException();
+                }))
+                .build();
     }
 
     @Override
