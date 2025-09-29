3df4de817ca192e98b58e00be531ac03a398ebda
rfcarmo <rfortunatoc@gmail.com>
2025-09-29 08:04:08 +0100

    11.7. Implementando client declarativo com HTTP Interface.

--- src/main/java/com/algaworks/algasensors/devicemanagement/api/client/SensorMonitoringClient.java
+++ src/main/java/com/algaworks/algasensors/devicemanagement/api/client/SensorMonitoringClient.java
@@ -2,12 +2,21 @@ package com.algaworks.algasensors.devicemanagement.api.client;
 
 import com.algaworks.algasensors.devicemanagement.api.model.SensorMonitoringOutput;
 import io.hypersistence.tsid.TSID;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PutExchange;
 
+@HttpExchange("/api/sensors/{sensorId}/monitoring")
 public interface SensorMonitoringClient {
 
-    void enableMonitoring(TSID sensorId);
+    @PutExchange("/enable")
+    void enableMonitoring(@PathVariable TSID sensorId);
 
-    void disableMonitoring(TSID sensorId);
+    @DeleteExchange("/enable")
+    void disableMonitoring(@PathVariable TSID sensorId);
 
-    SensorMonitoringOutput getDetails(TSID sensorId);
+    @GetExchange
+    SensorMonitoringOutput getDetails(@PathVariable TSID sensorId);
 }
